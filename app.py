5ebef7fcad34d5d6a85048fcea56fb4d9b13e268
Maurizio Arrigoni <maurizio.arrigoni@tiscali.it>
2021-02-25 09:24:38 +0100

    commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 # def hello():
-#     return "test_app_services"
+#     return "Hello world!"
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 def index():
-    return render_template('index.html')
+    return render_template('test_app_services.html')
