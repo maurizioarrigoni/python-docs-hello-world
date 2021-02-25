12077523480670600e7e8b92dc0060862b8a006c
Maurizio Arrigoni <maurizio.arrigoni@tiscali.it>
2021-02-25 08:20:03 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+# from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+# def hello():
+#     return "test_app_services"
+# https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
+def index():
+    return render_template('index.html')
